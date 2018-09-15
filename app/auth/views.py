13dcf29722743a274b28f05d6c327d19c914bb69
dk-denno <dennisveer27@gmail.com>
2018-09-15 12:07:30 +0300

    asdded emails folder

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,7 +4,7 @@ from flask import render_template,redirect,url_for
 from .forms import Signup, Login
 from ..models import User
 from flask_login import login_user,current_user
-
+from ..email import mail_message
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -16,6 +16,7 @@ def signup():
 
         db.session.add(users)
         db.session.commit()
+        mail_message("Welcome to pitches","email/welcome_user",users.email,user=user)
         return redirect(url_for('auth.login'))
 
     return render_template('auth/signup.html', signup=register)
